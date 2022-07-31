68b1ecfd55a3a2f6d95fef7eb0bc448195ce509b
rancheng <752050943@qq.com>
2022-07-31 08:44:51 +0800

    follow camera

--- cmake-build-release-dm-vio/test/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /tmp/tmp.ZfjOusJDzA/test/googletest
-# Build directory: /tmp/tmp.ZfjOusJDzA/cmake-build-release-dm-vio/test/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- cmake-build-release-dm-vio/test/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /tmp/tmp.ZfjOusJDzA/test/googletest/googlemock
-# Build directory: /tmp/tmp.ZfjOusJDzA/cmake-build-release-dm-vio/test/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
